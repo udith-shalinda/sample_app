84af7071a4651f025a6de2a1d416484d7100e626
udith-shalinda <udithshalinda2@gmail.com>
2020-03-19 14:45:52 +0530

    add client structure

---
+++ client/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from '../../server/node_modules/react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ client/src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+// import { useFindAllNotesQuery } from './generated-types';
+import CreateNote from './components/notes/CreateNote';
+import OneNote from './components/notes/OneNote';
+
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_DOGS = gql`
+  {
+    students {
+      id
+      first_name
+      last_name
+      email
+    }
+  }
+`;
+
+const App: React.FC = () => {
+  const { loading, error, data } = useQuery(GET_DOGS);
+  // const allNotes = useFindAllNotesQuery();
+  // allNotes.startPolling(2000);
+  console.log(data?.findAllStudents)
+ 
+  return (
+    <div>
+      <CreateNote></CreateNote>
+      <ul>
+        {
+          data && data.findAllStudents.map((student:any) => (
+            <OneNote key={student.id} id={student.id} title={student.first_name} description={student.last_name} comments={student.comments}></OneNote>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
